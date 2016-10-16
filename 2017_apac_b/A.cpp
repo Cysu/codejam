c3fa6fa7d48d980268211a12a36733ad87bed3c0
Cysu <st.cysu@gmail.com>
2016-10-16 11:22:57 +0800

    2017 apac b prob a

---
+++ 2017_apac_b/A.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdint>
+#include <cstdlib>
+#include <cmath>
+#include <climits>
+#include <cassert>
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <map>
+#include <stack>
+#include <queue>
+#include <set>
+
+using namespace std;
+
+void solve() {
+  int a, b;
+  cin >> a >> b;
+  a = min(a, b);
+  a = a % 2 == 0 ? a/2 * (1+a) : (1+a)/2 * a;
+  cout << a << endl;
+}
+
+int main() {
+  int T;
+  cin >> T;
+  for (int i = 0; i < T; ++i) {
+    printf("Case #%d: ", i + 1);
+    solve();
+  }
+}
